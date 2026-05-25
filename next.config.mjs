85b91f28c872c10e4a84a4a538040a7383c1c8eb
otavio-cyber <otavio@alligaremkt.com.br>
2026-05-25 12:21:57 -0300

    adjust frame ancenstors e xframe para embed externo

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://triunfae.com.br",
+          },
+        ],
+      },
+    ]
+  },
 }
 
-export default nextConfig
+export default nextConfig 
